499f3ea9c2e828f5bcd36e4e8273c072aba0ad09
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2024-10-02 22:33:29 -0300

    first commit

---
+++ bext_utils/symlink.py
@@ -0,0 +1,19 @@
+import subprocess
+from pathlib import Path
+
+
+from pyuac import main_requires_admin
+
+@main_requires_admin
+def main():
+    bl_ver = input("type blender version to make symlink")
+
+    cwd_path = Path().absolute()
+    ext_name = cwd_path.name
+    bl_addons_path = Path().home() / 'AppData' / 'Roaming' / 'Blender Foundation' / 'Blender' / bl_ver / 'scripts' / 'addons' / ext_name
+    bl_addons_path.symlink_to(cwd_path)
+
+
+if __name__ == "__main__":
+    main()
+
